ddcbc5696ff5c5750e75dd6260a5d0089bc004d6
Student <student@Makerss-MacBook-Air-2.local>
2020-04-28 16:30:54 +0100

    posted params

--- app.rb
+++ app.rb
@@ -14,10 +14,14 @@ end
 
 get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
-  erb(:index)
+  erb :index
 end
 
-get '/named-cat' do
+get '/cat-form' do
+  erb :cat_form
+end
+
+post '/named-cat' do
   p params
   @name = params[:name]
   erb :index
